0242d63207c39e5c87cbd230934486a941eab96c
BuildTools <unconfigured@null.spigotmc.org>
2025-11-21 08:02:48 +0200

    Basis

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,118 @@
 use std::{collections::HashMap, env, fs, path::Path, process};
 
+#[derive(Clone, PartialEq)]
+pub enum VarType {
+    Void,
+    Int,
+    Num,
+    Bool,
+    Char,
+    Callback(Vec<VarType>, Box<VarType>)
+}
+
 pub struct Variable {
-    name: String
+    name: String,
+    var_type: VarType
 }
 
 pub struct Function {
-    name: String
+    name: String,
+    args: Vec<Variable>,
+    return_type: VarType,
 }
 
-pub struct Module {
+pub struct Expression {
+    var_type: VarType
+}
+
+pub struct Object {
     name: String,
     globals: Vec<Variable>,
+    expressions: Vec<Expression>,
     functions: Vec<Function>,
-    modules: Vec<Module>,
+    objects: Vec<Object>,
     templates: Vec<String>
 }
 
-impl Module {
+impl VarType {
+    pub fn from_def(parser: &mut Parser) -> Self {
+        if parser.is_next("Int") {
+            return VarType::Int;
+        }
+        if parser.is_next("Num") {
+            return VarType::Num;
+        }
+        if parser.is_next("Bool") {
+            return VarType::Bool;
+        }
+        if parser.is_next("Char") {
+            return VarType::Char;
+        }
+        parser.error("Unknown type");
+        VarType::Void
+    }
+
+    pub fn to_script(&self) -> String {
+        match self {
+            VarType::Void => "Void".to_string(),
+            VarType::Int => "Int".to_string(),
+            VarType::Num => "Num".to_string(),
+            VarType::Bool => "Bool".to_string(),
+            VarType::Char => "Char".to_string(),
+            _ => "e".to_string()
+            // VarType::Callback(args, return_type) => format!("({}) -> {}", args.iter().map(|x| x.to_script()), return_type.to_script())
+        }
+    }
+}
+
+impl Expression {
+    pub fn from_def(parser: &mut Parser) -> Self {
+        parser.ensure_next("0");
+        Expression { var_type: VarType::Int }
+    }
+}
+
+impl Variable {
+    pub fn from_def(parser: &mut Parser) -> (Variable, Option<Expression>) {
+        let name: String = parser.next_word();
+        let mut result_var: Variable = Variable { name, var_type: VarType::Void };
+        let mut optional_expression = None;
+        let mut known_type: bool = false;
+        if parser.is_next(":") {
+            result_var.var_type = VarType::from_def(parser);
+            known_type = true;
+        }
+        if parser.is_next("=") {
+            let expr: Expression = Expression::from_def(parser);
+            if known_type {
+                if result_var.var_type != expr.var_type {
+                    parser.error(format!("Type mismatch during variable creation, expected '{}', got '{}'", result_var.var_type.to_script(), expr.var_type.to_script()).as_str());
+                }
+            } else {
+                result_var.var_type = expr.var_type.clone();
+            }
+            optional_expression = Some(expr);
+        } else if !known_type {
+            parser.error("Expected either a type decleration ':' or expression assigment '='");
+        }
+        (result_var, optional_expression)
+    }
+}
+
+impl Object {
     pub fn new() -> Self {
-        Module {
+        Object {
             name: String::new(),
             globals: Vec::new(),
+            expressions: Vec::new(),
             functions: Vec::new(),
-            modules: Vec::new(),
+            objects: Vec::new(),
             templates: Vec::new()
         }
     }
 
     pub fn from_def(parser: &mut Parser) -> Self {
-        let mut result: Module = Self::new();
+        let mut result: Object = Self::new();
         let name: String = parser.next_word();
         result.name = name;
         parser.ensure_next("{");
@@ -39,10 +123,17 @@ impl Module {
     }
 
     pub fn parse_next(&mut self, parser: &mut Parser) {
-        println!("l {} i {}", parser.cur_line, parser.index - parser.new_line_index);
-        if parser.is_next("module") {
-            self.modules.push(Module::from_def(parser));
-        } else {
+        let is_var = parser.is_next("var");
+        let is_val = parser.is_next("val");
+        let is_new_var = is_var ^ is_val;
+
+        if parser.is_next("object") {
+            self.objects.push(Object::from_def(parser));
+        } else if is_new_var {
+            let new_var = Variable::from_def(parser);
+            parser.ensure_next(";");
+        }
+        else {
             parser.error("Unknown expression");
         }
     }
@@ -71,7 +162,7 @@ impl Parser {
         &self.files[&self.cur_file_path]
     }
 
-    pub fn parse_file(&mut self, path: &str, main_module: &mut Module) {
+    pub fn parse_file(&mut self, path: &str, main_object: &mut Object) {
         if self.files.contains_key(path) {
             return;
         }
@@ -89,19 +180,19 @@ impl Parser {
         self.index = 0;
         self.cur_line = 0;
         self.files.insert(self.cur_file_path.clone(), contents.chars().collect());
-        self.parse(main_module);
+        self.parse(main_object);
         self.cur_file_path = saved_file_path;
         self.index = saved_index;
         self.cur_line = saved_cur_line;
     }
 
-    pub fn parse(&mut self, main_module: &mut Module) {
+    pub fn parse(&mut self, main_object: &mut Object) {
         while self.is_next("import") {
             let path: String = self.next_until(';');
-            self.parse_file(path.as_str(), main_module);
+            self.parse_file(path.as_str(), main_object);
         }
         while !self.is_finished() {
-            main_module.parse_next(self);
+            main_object.parse_next(self);
         }
     }
 
@@ -198,8 +289,8 @@ fn main() {
     }
 
     let mut parser: Parser = Parser::new();
-    let mut main_module: Module = Module::new();
-    main_module.name = "main_module".to_string();
+    let mut main_object: Object = Object::new();
+    main_object.name = "main_object".to_string();
 
-    parser.parse_file("main.cf", &mut main_module);
+    parser.parse_file("main.cf", &mut main_object);
 }
